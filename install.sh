104b790d97a7ded9efafedd911dd23fdb0eac097
stingsek <michalzadelek10@gmail.com>
2023-06-21 12:54:36 +0200

    minor change  install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ fi
 
 apt update
 apt-get install git python3 python3-pip python3-tk -y
+pip3 install tk
 git clone https://github.com/stingsek/hangman_game.git
 cd hangman_game
 rm install.sh
@@ -14,4 +15,3 @@ git remote remove origin
 rm -r .git
 pip3 install -r requirements.txt
 python3 ./app.py
-
